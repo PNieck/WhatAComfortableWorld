c7a749b11ce70b1b900f420d2153d41c813944bd
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2025-09-25 00:18:15 +0200

    Use data augmentation during preprocessing

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,7 @@ import sys
 import argparse
 import os
 import yaml
+import random
 try:
     from yaml import CLoader as Loader
 except ImportError:
@@ -12,6 +13,7 @@ except ImportError:
 
 from src.mat_file import from_mat_file 
 from src.sequence import to_sequence
+from src.data_augmentation import permutate, rotation, symmetry, SymmetryType, RotationAngle
 
 
 INVALID_FLOOR_PLANS_FILE = "data/invalid_floor_plans.txt"
@@ -34,26 +36,50 @@ def load_invalid() -> set[str]:
     return set(invalid_plans)
 
 
-def preprocess(file: str, start_idx: int, end_idx: int, data: np.ndarray, invalid_plans: set):
-    global max_tokens
+def random_enum_vector(enum_class, size):
+    return [random.choice(list(enum_class)) for _ in range(size)]
 
+
+def data_augmentation(plans, config):
+    new_plans = []
+    
+    for technique in config:
+        for k, v in technique.items():
+            cnt = int(v["percentage"] * len(plans))
+            samples = random.sample(plans, cnt)
+
+            if k == "permutation":
+                new_plans += [permutate(plan) for plan in samples]
+
+            elif k == "symmetry":
+                sym_types = random_enum_vector(SymmetryType, len(samples))
+                new_plans += [symmetry(plan, type) for plan, type in zip(samples, sym_types)]
+
+            elif k == "rotation":
+                rot_angles = random_enum_vector(RotationAngle, len(samples))
+                new_plans += [rotation(plan, angle) for plan, angle in zip(samples, rot_angles)]
+
+    plans = plans + new_plans
+    random.shuffle(plans)
+
+    return plans
+
+
+
+def preprocess(file: str, plans):
     os.makedirs(os.path.dirname(file), exist_ok=True)
     with open(file, mode='w+') as file:
-        for i, floor_plan in enumerate(data[start_idx:end_idx]):
-            if floor_plan.name in invalid_plans:
-                print(f"Skipping invalid file {floor_plan.name}")
-                continue
+        for i, floor_plan in enumerate(plans):
 
-            plan = from_mat_file(floor_plan)
-            seq = to_sequence(plan)
+            seq = to_sequence(floor_plan)
 
             for item in seq:
                 file.write(item)
 
             file.write("\n")
 
-            if (i+1) % 100 == 0 or i+1 == end_idx-start_idx:
-                print(f"{i+1}/{end_idx-start_idx}")
+            if (i+1) % 100 == 0 or i+1 == len(plans):
+                print(f"{i+1}/{len(plans)}")
 
 
 def parse_args(argv):
@@ -94,22 +120,32 @@ def main(argv):
     if "max_floor_plans" in prep_config:
         floor_plans_cnt = min(floor_plans_cnt, prep_config["max_floor_plans"])
 
+    data = data[0:floor_plans_cnt]
+
+    invalid_plans = load_invalid()
+    plans = [from_mat_file(plan) for plan in data if not plan.name in invalid_plans]
+
+    if "data_augmentation" in prep_config:
+        plans = data_augmentation(plans, prep_config["data_augmentation"])
+
+    floor_plans_cnt = len(plans)
+
     validate_cnt = int(floor_plans_cnt * prep_config["validate_size"])
     test_cnt = int(floor_plans_cnt * prep_config["test_size"])
     train_cnt = floor_plans_cnt - validate_cnt - test_cnt
 
-    invalid_plans = load_invalid()
+    
     print(f"Loaded {len(invalid_plans)} invalid plans")
 
     print("Preprocessing train dataset")
-    preprocess(paths_config["input_data"] + "/train.txt", 0, train_cnt, data, invalid_plans)
+    preprocess(paths_config["input_data"] + "/train.txt", plans[0:train_cnt])
 
     print("Preprocessing test dataset")
     test_end_idx = test_cnt + train_cnt
-    preprocess(paths_config["input_data"] + "/test.txt", train_cnt, test_end_idx, data, invalid_plans)
+    preprocess(paths_config["input_data"] + "/test.txt", plans[train_cnt: test_end_idx])
 
     print("Preprocessing validation dataset")
-    preprocess(paths_config["input_data"] + "/validation.txt", test_end_idx, floor_plans_cnt, data, invalid_plans)
+    preprocess(paths_config["input_data"] + "/validation.txt", plans[test_end_idx:floor_plans_cnt])
 
 
 if __name__ == "__main__":
