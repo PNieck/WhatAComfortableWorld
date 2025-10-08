65e24d8b6b8f4866ba29aaa70bed5fb488fba62b
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2025-10-08 15:10:59 +0200

    Update slurm scripts

--- train.sh
+++ train.sh
@@ -2,11 +2,12 @@
 #SBATCH --job-name=FloorPLanGenTraining   # Nazwa zadania
 #SBATCH --ntasks=1                        # Uruchomienie na jednym procesorze
 #SBATCH --mem=16gb                        # Wymagana pamięć RAM
-#SBATCH --time=01:00:00                   # maksymalny limit czasu DD-HH:MM:SS
+#SBATCH --time=10:00:00                   # maksymalny limit czasu DD-HH:MM:SS
 #SBATCH --output=training.log             # Log ze stdout i stderr
 #SBATCH --account=cadcam
 #SBATCH --gpus=1
+#SBATCH --partition=experimental,short
 
 
-venv\Scripts\activate
+source venv/bin/activate
 python3 train.py configs/main.yaml
