7924cc2e965c43cffec53923eb147fd9bdab6cff
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2025-10-18 21:14:14 +0200

    Update inference

--- inference.py
+++ inference.py
@@ -10,13 +10,8 @@ from transformers import AutoModelForCausalLM
 from datasets import load_dataset
 
 import floor_plan_tokenizer
-
-
-def draw_floor_plan(tokens):
-    tokenizer = floor_plan_tokenizer.FloorPlanTokenizer()
-    tokenized = tokenizer.tokenize(tokens)
-
-    print(tokenized)
+from src.drawing import draw_floor_plan
+from src.sequence import from_sequence
 
 
 def main():
@@ -37,14 +32,18 @@ def main():
     tokenizer = floor_plan_tokenizer.FloorPlanTokenizer()
     model = AutoModelForCausalLM.from_pretrained(paths_config["trained_model"])
 
-    valid_dataset = load_dataset(
-        "text",
-        data_files=[paths_config["input_data"] + "/validation.txt"]
-    )
+    # valid_dataset = load_dataset(
+    #     "text",
+    #     data_files=[paths_config["input_data"] + "/validation.txt"]
+    # )
 
-    prompt = "<Bound><Coord 193><Coord 103><Coord 197><Coord 103><Coord 197><Coord 208><Coord 153><Coord 208><Coord 153><Coord 180><Coord 60><Coord 180><Coord 60><Coord 75><Coord 68><Coord 75><Coord 68><Coord 78><Coord 134><Coord 78><Coord 134><Coord 51><Coord 197><Coord 51><Coord 197><Coord 73><Coord 193><Coord 73>"
+    prompt = "<Bound><Coord 95><Coord 64><Coord 95><Coord 52><Coord 119><Coord 52><Coord 119><Coord 68><Coord 185><Coord 68><Coord 185><Coord 148><Coord 165><Coord 148><Coord 165><Coord 195><Coord 109><Coord 195><Coord 109><Coord 205><Coord 72><Coord 205><Coord 72><Coord 64><Door><Coord 75><Coord 64><Coord 91><Coord 64>"
     inputs = tokenizer(prompt, return_tensors="pt",  return_token_type_ids=False).to(model.device)
     model.eval()
+
+    inputs["input_ids"] = inputs["input_ids"][:, 0:-1]
+    inputs["attention_mask"] = inputs["attention_mask"][:, 0:-1]
+
     with torch.no_grad():
         out = model.generate(
             **inputs,
@@ -55,10 +54,11 @@ def main():
         )
 
     print("\n=== Sample generation ===\n")
-    print(tokenizer.decode(out[0]))
+    sample = tokenizer.decode(out[0])
+    print(sample)
 
-    print("\n\n")
-    print(out[0])
+    floor_plan = from_sequence(sample, "generated")
+    draw_floor_plan(floor_plan)
 
 
 if __name__ == "__main__":
