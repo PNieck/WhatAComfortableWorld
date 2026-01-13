4abacd099d4070f9fdbb481d77a1ed35a288ef78
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2026-01-13 14:39:54 +0100

    something

--- create_imgs.py
+++ create_imgs.py
@@ -6,14 +6,23 @@ from src.drawing import draw_floor_plan_to_image
 
 from torch.utils.data import DataLoader
 
-dataset = load_floor_plans_dataset("data/gpt2_main/input", Split.VALID)
-data_loader = DataLoader(dataset["valid"]["text"], batch_size=1)
+# dataset = load_floor_plans_dataset("data/gpt2_main/input", Split.VALID)
+# data_loader = DataLoader(dataset["valid"]["text"], batch_size=1)
+
+with open("results/seqs/neighbor_loss.txt") as file:
+    lines = file.readlines()
+
+print(len(lines))
 
 i = 0
 
-for seq in data_loader:
-    floor_plan = from_sequence(seq[0])
-    draw_floor_plan_to_image(floor_plan, f"imgs/validation/{i}.png")
+for seq in lines:
+    try:
+        floor_plan = from_sequence(seq)
+    except:
+        continue
+
+    draw_floor_plan_to_image(floor_plan, f"imgs/neighbor_loss_legend_fixed/{i}.png")
     i += 1
 
     if i % 100 == 0:
